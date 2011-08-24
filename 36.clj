dad2b7e355d11377d92eb294699a284e24987ad6
Matt Furden <zolrath@gmail.com>
2011-08-24 04:56:48 -0700

    Added Benchmarks for Project 36

--- 36.clj
+++ 36.clj
@@ -4,8 +4,8 @@
         [criterium.core]))
 
 (defn is-binary-palindrome? [num]
-  (and (palindrome? num)
-       (palindrome? (to-binary num))))
+  (and (is-palindrome? num)
+       (is-palindrome? (to-binary num))))
 
 (defn solve36 []
   (reduce + (filter is-binary-palindrome? (range 1 1000000))))
