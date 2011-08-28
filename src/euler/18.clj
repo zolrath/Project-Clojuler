7fbae65cd223365454d973d4395e3d1b14aafa4e
Matt Furden <zolrath@gmail.com>
2011-08-28 02:59:34 -0700

    Finished comprehending 18.

--- src/euler/18.clj
+++ src/euler/18.clj
@@ -3,14 +3,12 @@
         [criterium.core])
   (:require [clojure.contrib.io :as io]))
 
-(def triangle
-(slurp "/users/furd/triangle.txt")) 
+(defn slurp-triangle [file]
+  (for [line (line-seq (io/reader file))]
+    (map #(Integer/parseInt %) (re-seq #"[\d.]+" line))))
 
-(with-open [rdr (io/reader "/users/furd/triangle.txt")]
-(reduce conj [] (line-seq rdr)))
-
-(io/with-in-reader (io/file "/users/furd/triangle.txt") (read) (read))
-
+; Ended up stealing this implementation while looking into how to
+; parse files properly. It's so elegant, how could I not?!
 (defn merge-rows [row-n row-n-1]
   (map (fn [a [b c]] (+ a (max b c)))
        row-n-1
@@ -18,11 +16,6 @@
  
 (defn max-path [triangle]
   (reduce merge-rows (reverse triangle)))
- 
-(defn slurp-triangle [filepath]
-  (with-open [fr  (-> filepath java.io.File. java.io.FileReader.)
-              fbr (java.io.BufferedReader. fr)]
-    (doall (map (fn [s] (doall (map #(Integer/parseInt %) (.split s " ")))) (line-seq fbr)))))
 
-(max-path
-  (slurp-triangle "/users/furd/triangle.txt"))
+(defn solve18 []
+  (max-path (slurp-triangle "src/euler/txt/triangle.txt")))
