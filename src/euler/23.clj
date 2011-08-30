a261ba9d4aa2c270e93ee3a1c7a2a115600f9cd1
Matt Furden <zolrath@gmail.com>
2011-08-30 10:45:28 -0700

    Making progress on Project 23

--- src/euler/23.clj
+++ src/euler/23.clj
@@ -1,12 +1,31 @@
 (ns euler.23
   (:use [euler.core]
-        [criterium.core]))
+        [criterium.core]
+        [clojure.set]))
 
 (defn is-abundant? [num]
-  (> (reduce + (sum-divisors num)) num))
+  (> (sum-divisors num) num))
 
+(defn is-abundant?b [num]
+  (> (inefficient-sum-divisors num) num))
+;; CONFUSED AS HELL
+;; Why is sum-divisors so much faster when performed by itself, but
+;; when used on a range, my slower sum-dividers becomes... faster?
 ;; Limit 20161
 ;; After 48, all even numbers are sum-abundent
 
-(defn sum-abundent [20161]
-  (reduce + (filter is-abundant? (range 1 limit))))
+(defn abundantlist [start end]
+  (filter is-abundant? (range start end)))
+
+(defn abundantlistb [start end]
+  (filter is-abundant?b (range start end)))
+
+
+(defn abundantsums []
+  (let [list (for [a (abundantlist 1 200)
+                   b (abundantlist 1 (inc a))]
+               (+ a b))]
+    (set list)))
+
+(defn nonabundant []
+  (difference (set (doall (range 1 500))) (abundantsums)))
--- src/euler/core.clj
+++ src/euler/core.clj
@@ -12,11 +12,17 @@
 
 (defn is-prime? [n]
   "Returns true if given number is prime"
-   (and (not= 0 (mod n 2)) (zero? (count (filter #(zero? (rem n %)) (range 3 (inc (sqrt n)) 2))))))
+  (and (not= 0 (mod n 2))
+       (zero? (count (filter #(zero? (rem n %)) (range 3 (inc (sqrt n)) 2))))))
 
 (defn triangle-num [n]
   "Returns nth triangle number"
-  (/ (+ (square n) n) 2))
+  (-> (square n) (+ n) (/ 2)))
+
+(def triangles (lazy-cat [0] (map + triangles (iterate inc 1))))
+
+(defn n-triangle-nums [n]
+  (take n triangles))
 
 (defn to-binary [num]
   "Converts given base 10 number to binary"
@@ -48,8 +54,8 @@
 (defn primes-to [n]
   (take-while #(< % n) primes))
 
-(defn prime-factors-of [num]
-  "Returns the prime factors of a given number."
+(defn thorough-prime-factors [num]
+  "Returns the prime factors of a given number. 20 => (5 2 2)"
   (loop [a (primes-to (inc (/ num 2)))
          number num 
          factorlist '()]
@@ -59,19 +65,30 @@
         (recur (rest a) number factorlist))
       factorlist)))
 
+(defn prime-factors [num]
+  "Returns the prime factors of a given number. 20 + (5 2)"
+  (filter #(zero? (mod num %)) (primes-to (/ num 2))))
+
 (defn divisors [num]
   "Returns a list of the Proper Divisors of given number"
   (filter #(zero? (mod num %)) (range 1 (inc (/ num 2)))))
 
 (defn sum-divisors [num]
+  (reduce + (divisors num)))
+
+(defn inefficient-sum-divisors [num]
   "Returns sum of Proper Divisors (not including number itself)"
-  (let [prime-factors (partition-by identity (prime-factors-of num))]
+  (let [prime-factors (partition-by identity (thorough-prime-factors num))]
     (let [sum (reduce * (map #(if (> (count %) 1)
                                 (-> (first %) (expt (inc (count %))) (dec) (/ (dec (first %))))
                                 (inc (first %)))
                              prime-factors))]
       (if (= sum 1) 1 (- sum num)))))
 
-(defn divisors-count [num]
+(defn inefficient-divisors-count [num]
   "Multiplies exponents of prime divisors(each incremented by one) to give number of divisors"
-  (reduce * (map inc (map count (partition-by identity (prime-factors-of num))))))
+  (reduce * (map inc (map count (partition-by identity (thorough-prime-factors num))))))
+
+(defn divisors-count [n]
+  "Returns total number of divisors of given number"
+  (* 2 (count (filter #(zero? (rem n %)) (range 1 (Math/sqrt n))))))
